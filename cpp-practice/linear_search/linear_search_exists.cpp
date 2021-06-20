222d04e9d7d96e4b014e4682ff1e8fa8d8763152
Kazumasa Hirata <p.m.byzance0817@gmail.com>
2021-06-21 08:20:54 +0900

    Practiced linear search

---
+++ cpp-practice/linear_search/linear_search_exists.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int N, v;
+  cin >> N >> v;
+  vector<int> a(N);
+  for (int i = 0; i < N; ++i) cin >> a[i];
+
+  bool exist = false;
+  for (int i = 0; i < N; ++i) {
+	if (a[i] == v) {
+	  exist = true;
+	  break;
+	}
+  }
+
+  if (exist) {
+	cout << "Yes" << endl;
+  } else {
+	cout << "No" << endl;
+  }
+}
