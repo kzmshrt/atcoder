861aa2f828a972b8d56d6253f740858a86c5f615
Kazumasa Hirata <p.m.byzance0817@gmail.com>
2021-06-24 01:04:39 +0900

    Add assert_eq practice

--- rust/src/assert.rs
+++ rust/src/assert.rs
@@ -1,11 +1,16 @@
 fn main() {
     div(55);
     div_euclid(-123);
+    round(10, 98);
 }
 
 fn div(x: i32) {
     let r = x % 10;
-    assert!(0 <= r && r < 10, "割った余りが想定の範囲から外れています");
+    assert!(
+        0 <= r && r < 10,
+        "割った余りが {} で、想定の範囲から外れています",
+        r
+    );
     println!("あまりは {}", r);
 }
 
@@ -14,4 +19,9 @@ fn div_euclid(x: i32) {
     let r = x.rem_euclid(10);
     assert!(0 <= r && r < 10);
     println!("あまりは {}", r);
+}
+
+fn round(x: i32, y: i32) {
+    let rounded = x / y * y;
+    assert_eq!(rounded % y, 0);
 }
