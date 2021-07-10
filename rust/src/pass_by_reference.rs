09a2051a88cc4a973ab145396f7f51ba98ab2542
Kazumasa Hirata <p.m.byzance0817@gmail.com>
2021-07-11 01:19:36 +0900

    Add pass by reference practice

--- rust/src/pass_by_reference.rs
+++ rust/src/pass_by_reference.rs
@@ -1,6 +1,8 @@
 fn main() {
     practice_vector_moved_to_function();
     practice_pass_by_reference();
+    practice_pass_by_reference_mutable();
+    practice_dbg_macro();
 }
 
 fn practice_vector_moved_to_function() {
@@ -37,4 +39,25 @@ fn practice_pass_by_reference() {
     let vector = vec![20, 80, 60, 40];
     let s = sum(&vector);
     println!("sum of {:?}: {}", vector, s);
+}
+
+fn practice_pass_by_reference_mutable() {
+    fn double(x: &mut i32) {
+        *x *= 2;
+    }
+
+    let mut hoge = 10;
+    double(&mut hoge);
+    assert_eq!(hoge, 20);
+    double(&mut hoge);
+    assert_eq!(hoge, 40);
+}
+
+fn practice_dbg_macro() {
+    let mut x = 0;
+    for i in 18..=20 {
+        x += i;
+        dbg!(x);
+    }
+    println!("{}", x);
 }
